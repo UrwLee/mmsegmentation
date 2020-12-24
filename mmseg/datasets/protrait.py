9399bfa97882e82bdee8c1a26aea14a4ef3c5ebe
UrwLee <979634820@qq.com>
2020-12-24 15:52:18 +0800

    base

---
+++ mmseg/datasets/protrait.py
@@ -0,0 +1,22 @@
+import os.path as osp
+
+from .builder import DATASETS
+from .custom import CustomDataset
+
+
+@DATASETS.register_module()
+class PortraitDataset(CustomDataset):
+    """Portrait dataset.
+
+    Args:
+        split (str): Split txt file for Portrait.
+    """
+
+    CLASSES = ('background','person')
+
+    PALETTE = [[0,0,255],[255,0,0]]
+
+    def __init__(self, split, **kwargs):
+        super(PortraitDataset, self).__init__(
+            img_suffix='.jpg', seg_map_suffix='.png', split=split, **kwargs)
+        assert osp.exists(self.img_dir) and self.split is not None
